50b6c4b73d2578fde8056488a0e874ac49de3031
Hosea Kibet <iankibet@Hoseas-MacBook-Air.local>
2025-11-26 08:19:59 +0300

    set default streamline url

--- src/plugins/streamline.js
+++ src/plugins/streamline.js
@@ -1,6 +1,6 @@
 const streamline = {
     install(app, options) {
-        app.provide('streamlineUrl', options.streamlineUrl)
+        app.provide('streamlineUrl', options.streamlineUrl ?? '/api/streamline')
         // app.provide('streamlineHeaders', options.streamlineHeaders)
         app.provide('enableCache', options.enableCache)
     }
